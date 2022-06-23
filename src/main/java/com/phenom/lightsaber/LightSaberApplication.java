f11113f1f82ba240db09ce1552ba1dd38837faae
phenom_soumyadeep_roy <soumyadeep.roy@phenompeople.com>
2022-06-23 19:04:48 +0530

    Init

---
+++ src/main/java/com/phenom/lightsaber/LightSaberApplication.java
@@ -0,0 +1,13 @@
+package com.phenom.lightsaber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightSaberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightSaberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phenom/lightsaber/LightSaberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phenom.lightsaber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightSaberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
